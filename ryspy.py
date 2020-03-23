2a240f2dc9260ab7477929dd94ae0d9fd977ae7d
Ryan <r.hennings11@gmail.com>
2020-03-22 23:15:21 -0700

    - init repo - add servo board api

---
+++ ryspy.py
@@ -0,0 +1,27 @@
+import time
+from SunFounder_PCA9685 import Servo
+
+TILT_CHAN = 0
+PAN_CHAN = 1
+PI_SERVO = []
+
+# Add a read command instead of global
+
+
+def set_pan(degree):
+    if degree < 0 or degree > 180:
+        raise ValueError("Degree must be in boundry > 0 && < 180")
+
+    PI_SERVO[PAN_CHAN].write(degree)
+
+def pan_left(degree=5):
+    print()
+
+
+if __name__ == "__main__":
+    PI_SERVO.append(Servo.Servo(TILT_CHAN))
+    PI_SERVO.append(Servo.Servo(PAN_CHAN))
+
+    while True:
+        x = input("Enter Degree: ")
+        pan(x)
